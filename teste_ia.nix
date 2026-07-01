6e317cab25897c83cd9e4cc64b29de44fe5f3777
Leo T. <thedandare@gmail.com>
2026-07-01 11:20:01 -0300

    Remova configuração do firewall no teste_ia.nix

--- teste_ia.nix
+++
@@ -1,6 +0,0 @@
-{ config, pkgs, ... }: {
-  # Ativa o firewall do sistema
-  networking.firewall.enable = true;
-  # Permite portas específicas para testes
-  networking.firewall.allowedTCPPorts = [ 80 443 ];
-}
