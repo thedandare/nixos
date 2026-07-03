6f7290381a30f92d6e81f974b398374b8ed24544
Leonardo Taschetto <leonardotaschetto@gmail.com>
2026-07-03 14:56:50 -0300

    ocnix: fix DNS with chattr +i resolv.conf in runcmd

--- ocnix/configuration.nix
+++ ocnix/configuration.nix
@@ -50,7 +50,9 @@ let
     runcmd:
       - ip addr add 10.10.10.2/24 dev eth0 || true
       - ip route add default via 10.10.10.1 || true
+      - chattr -i /etc/resolv.conf || true
       - echo "nameserver 1.1.1.1" > /etc/resolv.conf
+      - chattr +i /etc/resolv.conf
       - apt-get update
       - apt-get install -y snapd jq curl
       - systemctl unmask snapd.service snapd.socket || true
