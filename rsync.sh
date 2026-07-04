34047658f59eed42a2a9959b64a079bc35fb4084
Leo T. <thedandare@gmail.com>
2026-07-03 23:07:45 -0300

    Remova conflitos e ajuste comando do rsync.

--- rsync.sh
+++ rsync.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-<<<<<<< HEAD
 sudo rsync -aHAXxv --exclude='**/.git/' --progress --stats /etc/nixos/ /osnix/nixos/leonix
-=======
-sudo rsync -aHAXxv --exclude='**/.git/' --delete --progress --stats /etc/nixos/ /osnix/nixos/ocnix
->>>>>>> 5a59d3270954644fad80cf919e5abc44ac575afc
